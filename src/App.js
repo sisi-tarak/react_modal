bc3b1cac7798515b4f6cecf1513c152c7a4cdacb
Sisindri Singamsetti <sisindrisingamsetti@gmail.com>
2023-02-07 09:17:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import './App.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import NotFound from './Components/NotFound';
 import { Provider } from './Components/Context';
-import ScrollToTop from './Components/ScrollToTop';
-import RecommendationSection from './Components/RecommendationSection';
-import AddRecommendation from './Components/AddRecommendaion';
+import CardSection from './Components/CardSection';
 
 function App() {
   return (
     <Provider>
         <BrowserRouter>
-        <ScrollToTop/>
-          <Navbar/>
-            <Switch>
               <Route exact path='/'>
-                <RecommendationSection/>
+                <CardSection/>
               </Route>
-              <Route exact path='/recommendation/add'>
-                <AddRecommendation/>  
-              </Route>  
-              <Route component={NotFound} />
-            </Switch>
-          <Footer />
         </BrowserRouter>
     </Provider>
   );
